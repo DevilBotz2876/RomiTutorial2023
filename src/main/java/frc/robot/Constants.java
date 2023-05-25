380802287d1b6872c983cedbb711d2de1c6dcc5c
Nilesh Agarwalla <nilesh@alum.mit.edu>
2023-05-25 18:54:47 -0400

    Add SysId drivetrain constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,4 +19,36 @@ public final class Constants {
     static final boolean REPLAY_MODE = false;
   }
   // END: Setup AdvantageKit
+
+  // START: Setup SysId
+  public final class SysId {
+    public final class Drive {
+      public static final double wheelRadiusMeters = 0.070 / 2; // 70mm diameter wheels
+      public static final double trackWidthMeters = 0.14692;
+
+      public static final double maxSpeedMetersPerSecond = 0.65;
+      public static final double maxRotationRadiansPerSecond = 18;
+
+      public static final double combinedksVolts = 0.23565;
+      public static final double combinedkvVoltSecondsPerMeter = 10.41;
+      public static final double combinedkaVoltSecondsSquaredPerMeter = 0.81878;
+
+      public static final double leftKsVolts = 0.15501;
+      public static final double leftKvVoltSecondsPerMeter = 10.795;
+      public static final double leftKaVoltSecondsSquaredPerMeter = 0.22663;
+
+      public static final double leftKpVelocity = 0.71328;
+      public static final double leftKiVelocity = 0;
+      public static final double leftKdVelocity = 0;
+
+      public static final double rightKsVolts = 0.27801;
+      public static final double rightKvVoltSecondsPerMeter = 10.039;
+      public static final double rightKaVoltSecondsSquaredPerMeter = 0.22158;
+
+      public static final double rightKpVelocity = 0.78136;
+      public static final double rightKiVelocity = 0;
+      public static final double rightKdVelocity = 0;
+    }
+    // END: Setup SysId
+  }
 }
