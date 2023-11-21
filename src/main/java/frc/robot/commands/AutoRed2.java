09d4263035cc0fc71b010ff547e39d036f9acfb6
Ethan Porter <emanporter2009@gmail.com>
2023-11-20 19:42:10 -0500

    trying to fix auto

--- src/main/java/frc/robot/commands/AutoRed2.java
+++ src/main/java/frc/robot/commands/AutoRed2.java
@@ -7,10 +7,10 @@ import frc.robot.subsystems.Drivetrain;
 public class AutoRed2 extends SequentialCommandGroup {
     public AutoRed2(Drivetrain drivetrain) {
       addCommands(
-          new DriveDistance(-0.5, 1, drivetrain),
+          new DriveDistance(0.25, 7.5, drivetrain),
           new TurnDegrees(0.5, 90, drivetrain),
-          new DriveDistance(-0.5, 1, drivetrain),
-          new TurnDegrees(0.5, 90, drivetrain),
-          new DriveDistance(-0.5, 1, drivetrain));
+          new DriveDistance(0.25, 14, drivetrain)
+          //new TurnDegrees(0.5, 90, drivetrain),
+          /*new DriveDistance(0.25, 1, drivetrain)*/);
     }
   }
--- src/main/java/frc/robot/commands/AutoRed3.java
+++ src/main/java/frc/robot/commands/AutoRed3.java
@@ -7,10 +7,10 @@ import frc.robot.subsystems.Drivetrain;
 public class AutoRed3 extends SequentialCommandGroup {
     public AutoRed3(Drivetrain drivetrain) {
       addCommands(
-          new DriveDistance(-0.5, 1, drivetrain),
+          new DriveDistance(1, 16, drivetrain));
+          /*new TurnDegrees(0.5, -90, drivetrain),
+          new DriveDistance(-1, 22, drivetrain),
           new TurnDegrees(0.5, 90, drivetrain),
-          new DriveDistance(-0.5, 1, drivetrain),
-          new TurnDegrees(0.5, 90, drivetrain),
-          new DriveDistance(-0.5, 1, drivetrain));
+          new DriveDistance(-1, 16, drivetrain));*/
     }
   }
