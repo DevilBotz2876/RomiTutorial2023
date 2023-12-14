bec7df071d63c0c0342cde24820465969345fe1b
Braden Santiano <bradensanti@outlook.com>
2023-12-14 18:48:51 -0500

    FinishedRomi

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -16,7 +16,7 @@ public class AutonomousDistance extends SequentialCommandGroup {
    */
   public AutonomousDistance(Drivetrain drivetrain) {
     addCommands(
-        new DriveDistance(-0.5, 10, drivetrain),
+        new DriveDistance(-0.6, 12, drivetrain),
         new TurnDegrees(-0.5, 180, drivetrain),
         new DriveDistance(-0.5, 10, drivetrain),
         new TurnDegrees(0.5, 180, drivetrain));
